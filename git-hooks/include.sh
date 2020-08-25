6736add5231ebbe2460ca056d677295345e1b8d2
pascal <pascal.morin@codeenigma.com>
2020-08-25 12:52:15 +0200

    Fix loop being broken.

--- git-hooks/include.sh
+++ git-hooks/include.sh
@@ -2,7 +2,7 @@
 set -eu
 # Perform actual hooks.
 GIT_DIR=$(git rev-parse --show-toplevel 2> /dev/null)
-for file in $(find "$GIT_DIR/$0.d" -name "*.hook" -print -quit); do 
+for file in $(find "$GIT_DIR/$0.d" -name "*.hook"); do 
   chmod +x "$file"
-  "$file" "$@"
+  /bin/sh "$file" "$@"
 done
