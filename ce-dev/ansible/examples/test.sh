d8d16295b6b41ee6ff10f92e464141133e41778a
gregharvey <greg.harvey@gmail.com>
2021-02-05 18:19:25 +0100

    Revert "Fixing test.sh to explicitly call bash."
    This reverts commit 521279ebc16a4c4459c981bfb813cf6aa4d4f3ad.

--- ce-dev/ansible/examples/test.sh
+++ ce-dev/ansible/examples/test.sh
@@ -18,7 +18,7 @@ init_ce_dev(){
 # @param $1
 # Example name.
 build_example(){
-  PROVISION_CMD="/bin/bash /home/ce-dev/ce-provision/scripts/provision.sh"
+  PROVISION_CMD="/bin/sh /home/ce-dev/ce-provision/scripts/provision.sh"
   PROVISION_CMD="$PROVISION_CMD --repo dummy --branch dummy --workspace /home/ce-dev/ce-provision/ce-dev/ansible --playbook examples/$1/$1.yml"
   # shellcheck disable=SC2086
   sudo docker exec -t --workdir /home/ce-dev/ce-provision --user ce-dev provision-controller $PROVISION_CMD
