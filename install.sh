dcc7bb5cd1c02240ad0251cf0f722171ff7c3f7c
tim <tymofii.sobchenko@codeenigma.com>
2025-09-11 14:37:05 +0300

    pin_ansible_version

--- install.sh
+++ install.sh
@@ -84,6 +84,7 @@ FIREWALL="true"
 AWS_SUPPORT="false"
 IS_LOCAL="false"
 SERVER_HOSTNAME=$(hostname)
+ANSIBLE_VERSION="<12"
 
 # Parse options.
 parse_options "$@"
@@ -146,7 +147,7 @@ fi
 /usr/bin/echo "-------------------------------------------------"
 /usr/bin/su - "$CONTROLLER_USER" -c "/usr/bin/python3 -m venv /home/$CONTROLLER_USER/ce-python"
 /usr/bin/su - "$CONTROLLER_USER" -c "/home/$CONTROLLER_USER/ce-python/bin/python3 -m pip install --upgrade pip"
-/usr/bin/su - "$CONTROLLER_USER" -c "/home/$CONTROLLER_USER/ce-python/bin/pip install ansible netaddr python-debian"
+/usr/bin/su - "$CONTROLLER_USER" -c "/home/$CONTROLLER_USER/ce-python/bin/pip install ansible$ANSIBLE_VERSION netaddr python-debian"
 if [ "$AWS_SUPPORT" = "true" ]; then
   /usr/bin/su - "$CONTROLLER_USER" -c "/home/$CONTROLLER_USER/ce-python/bin/pip install boto3"
 fi
